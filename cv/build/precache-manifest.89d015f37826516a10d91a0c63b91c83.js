5b84b2857840b8b1d536b41d20f41c3786d3b5bc
Borja Arias <borja@relive.com>
2023-01-02 00:38:50 +0100

    CV

---
+++ cv/build/precache-manifest.89d015f37826516a10d91a0c63b91c83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d360249501c58966b8bb3a3178c9ee83",
+    "url": "/css.html"
+  },
+  {
+    "revision": "74cf13a9fc078c2cf607906fa282fb0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b217db5240b657f80908a490b72c1f46",
+    "url": "/js.html"
+  },
+  {
+    "revision": "4e797448928bff3b2c60",
+    "url": "/static/css/main.201cb2ed.css"
+  },
+  {
+    "revision": "4e797448928bff3b2c60",
+    "url": "/static/js/main.a19af499.js"
+  },
+  {
+    "revision": "fcf9adb2e4f1fc9117872fab312907c0",
+    "url": "/static/js/main.a19af499.js.LICENSE.txt"
+  }
+]);
